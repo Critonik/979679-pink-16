dc9329641125728476564e257b307cd7903b06bd
Critonik <42637808+Critonik@users.noreply.github.com>
2019-05-17 19:49:18 +0500

    немного право

--- source/js/script.js
+++ source/js/script.js
@@ -1,18 +1,17 @@
 var button = document.querySelector(".main-nav_toggle");
 var menu = document.querySelector(".main-nav_wrapper");
-var head = document.querySelector(".page-header_top")
-
+var head = document.querySelector(".page-header_top");
 
 button.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    if (menu.classList.contains("menu-show")) {
-      menu.classList.remove("menu-show");
-      head.classList.remove("menu-position");
-      button.classList.remove("main-nav_toggle_cross");
-    }
-    else {
-      menu.classList.add("menu-show");
-      head.classList.add("menu-position");
-      button.classList.add("main-nav_toggle_cross");
-    }
-  });
+  evt.preventDefault();
+  if (menu.classList.contains("menu-show")) {
+    menu.classList.remove("menu-show");
+    head.classList.remove("menu-position");
+    button.classList.remove("main-nav_toggle_cross");
+  }
+  else {
+    menu.classList.add("menu-show");
+    head.classList.add("menu-position");
+    button.classList.add("main-nav_toggle_cross");
+  }
+});
