762abf524b5da9301e9cae6477f6574974765bd9
Critonik <42637808+Critonik@users.noreply.github.com>
2019-05-22 00:05:02 +0500

    подгонка под критерии

--- source/js/script.js
+++ source/js/script.js
@@ -2,16 +2,17 @@ var button = document.querySelector(".main-nav_toggle");
 var menu = document.querySelector(".main-nav_wrapper");
 var head = document.querySelector(".page-header_top");
 
+menu.classList.add("menu-show");
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
   if (menu.classList.contains("menu-show")) {
     menu.classList.remove("menu-show");
-    head.classList.remove("menu-position");
-    button.classList.remove("main-nav_toggle_cross");
+    head.classList.add("menu-position");
+    button.classList.add("main-nav_toggle_cross");
   }
   else {
     menu.classList.add("menu-show");
-    head.classList.add("menu-position");
-    button.classList.add("main-nav_toggle_cross");
+    head.classList.remove("menu-position");
+    button.classList.remove("main-nav_toggle_cross");
   }
 });
