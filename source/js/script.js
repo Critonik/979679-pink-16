c58c24c5ed02eb98f842be013eec094f3f3d2f21
Critonik <42637808+Critonik@users.noreply.github.com>
2019-05-23 23:10:16 +0500

    под пиксель перфект

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,12 @@
 var button = document.querySelector(".main-nav_toggle");
 var menu = document.querySelector(".main-nav_wrapper");
 var head = document.querySelector(".page-header_top");
+var logo = document.querySelector(".logo");
 
 button.classList.add("main-nav_burger")
 menu.classList.add("menu-show");
+logo.classList.remove("logo-background_color");
+
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
   if (menu.classList.contains("menu-show")) {
